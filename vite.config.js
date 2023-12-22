2995f6defbe3eadc1897a3cc6aec8d390aef7da7
Yulia Glagola <julichis@mail.ru>
2023-12-22 21:39:15 +0400

    added vitest coverage

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: '.src//tests/setup.js',
+    setupFiles: './src/tests/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
