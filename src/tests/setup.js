2304f91cbce561e9940b520de692b528b6df9a67
Yulia Glagola <julichis@mail.ru>
2023-12-23 19:19:40 +0400

    configured msw

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,8 +1,13 @@
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "../mocks/node";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+  server.resetHandlers();
 });
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
