6a4e6ef9fbc88441ce6dc00280145081c2604b4a
Yulia Glagola <julichis@mail.ru>
2023-12-25 19:55:21 +0400

    added test for useCustomQuery hook (fetches mock data correctly)

---
+++ src/hooks/useCustomQuery.test.jsx
@@ -0,0 +1,36 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+import { test, expect } from 'vitest';
+import { useCustomQuery } from './useCustomQuery';
+import { mockCurrencies } from '@/mocks/mockCurrencies';
+import { mockRates } from '@/mocks/mockRates';
+
+test('intercept fetch for testing', async () => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      });
+    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+    const { result } = renderHook(() => useCustomQuery(), { wrapper });
+
+    await waitFor(() => expect(result.current.currenciesQuery.isSuccess).toBe(true));
+    await waitFor(() => expect(result.current.ratesQuery.isSuccess).toBe(true));
+});
+
+test('fetches mocked data correctly', async () => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      });
+    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+    const { result } = renderHook(() => useCustomQuery(), { wrapper });
+    console.log(result.current.ratesQuery.data)
+    await waitFor(() => expect(result.current.currenciesQuery.data).toEqual(mockCurrencies));
+    await waitFor(() => expect(result.current.ratesQuery.data).toEqual(mockRates.rates));
+})
