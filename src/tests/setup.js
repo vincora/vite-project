e94392146ed3259ac8fe0dd07baed2015f25b94e
Yulia Glagola <julichis@mail.ru>
2024-01-03 16:49:47 +0400

    added configs for click events

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
-import { beforeAll, afterEach, afterAll } from 'vitest';
+import { beforeAll, afterEach, afterAll, vi } from 'vitest';
+
 import { server } from '../mocks/node';
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
@@ -9,5 +10,10 @@ afterEach(() => {
     server.resetHandlers();
 });
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+    window.HTMLElement.prototype.scrollIntoView = vi.fn();
+    window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+    window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+    server.listen();
+});
 afterAll(() => server.close());
