1c7e5da6b0de9561d43cbcd53dbe044e1d466a20
Yulia Glagola <julichis@mail.ru>
2024-01-06 19:50:19 +0400

    fixed bug in format number function

--- src/lib/formatNumber.js
+++ src/lib/formatNumber.js
@@ -2,9 +2,11 @@ export const formatNumber = (num) => {
     if (num < 0) {
         throw new Error('Only positive numbers accepted');
     }
-    if (num >= 1) {
-        return num.toFixed(2);
+    const numString = num.toFixed(20);
+
+    if (num < 0.01){
+        const decimalIndex = numString.substring(2).search(/0[^0]/) + 1;
+        return num.toFixed(decimalIndex + 2);
     }
-    const decimalIndex = String(num).substring(2).search(/0[^0]/) + 1;
-    return num.toFixed(decimalIndex + 2);
+    return num.toFixed(2);
 };
