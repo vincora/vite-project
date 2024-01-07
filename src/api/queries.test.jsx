25230e228d30384306e3fd84e7bfa3c9b3e23fb3
Yulia Glagola <julichis@mail.ru>
2024-01-07 20:12:50 +0400

    refactoring

--- src/api/queries.test.jsx
+++ src/api/queries.test.jsx
@@ -1,62 +1,32 @@
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 import { test, expect } from 'vitest';
 
 import { mockCurrencies } from '@/mocks/mockCurrencies';
 import { mockRates } from '@/mocks/mockRates';
+import { customQueryWrapper } from '@/tests/testQueryWrapper';
 
 import { useApiCurrencies, useApiRates } from './queries';
 
 test('intercepts useApiCurrencies fetch for testing', async () => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                retry: false,
-            },
-        },
-    });
-    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-    const { result } = renderHook(() => useApiCurrencies(), { wrapper });
+    const { result } = renderHook(() => useApiCurrencies(), { wrapper: customQueryWrapper() });
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
 });
 
 test('intercepts useApiRates fetch for testing', async () => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                retry: false,
-            },
-        },
-    });
-    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-    const { result } = renderHook(() => useApiRates(), { wrapper });
+    const { result } = renderHook(() => useApiRates(), { wrapper: customQueryWrapper() });
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
 });
 
 test('fetches rates mocked data correctly', async () => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                retry: false,
-            },
-        },
-    });
-    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-    const { result } = renderHook(() => useApiRates(), { wrapper });
+    const { result } = renderHook(() => useApiRates(), { wrapper: customQueryWrapper() });
+
     await waitFor(() => expect(result.current.data).toEqual(mockRates.rates));
 });
 
 test('fetches currencies mocked data correctly', async () => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                retry: false,
-            },
-        },
-    });
-    const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-    const { result } = renderHook(() => useApiCurrencies(), { wrapper });
+    const { result } = renderHook(() => useApiCurrencies(), { wrapper: customQueryWrapper() });
+
     await waitFor(() => expect(result.current.data).toEqual(mockCurrencies));
 });
